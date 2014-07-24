b4723e854e2fbfcdcc97aedf91e709c597f1b4d9
Chase Gosingtian <c.gosingtian@klab.com>
2014-07-24 11:40:43 +0800

    Initial commit.

--- TypingBattle/KLBAppDelegate.h
+++ TypingBattle/KLBAppDelegate.h
@@ -6,6 +6,16 @@
 //  Copyright (c) 2014 KLab Cyscorpions, Inc. All rights reserved.
 //
 
+/*
+ * Typing Battle:
+ *  The player must be able to type the word displayed within the time limit
+ *   Success grants 1 point
+ *   Failure grants -1 point
+ *  
+ 
+ 
+ */
+
 #import <Cocoa/Cocoa.h>
 
 @interface KLBAppDelegate : NSObject <NSApplicationDelegate>
